4bee6f762f0ecefa21803733b6a8d9d6fd54b578
Sean Grant <seangrant118@gmail.com>
2018-10-17 16:26:01 -0400

    routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,5 +5,11 @@ const friendsData = require("../data/friends");
 // routing 
 
 module.exports = function(app) {
-  
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  })
+
+  app.post("/api/friends", function(req, res) {
+    friendsData.push(req.body);
+  })
 }
