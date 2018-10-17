9a5f456dcf462f5dde152bb4f8ae1e50fc6515e0
Sean Grant <seangrant118@gmail.com>
2018-10-17 16:15:50 -0400

    server dummy data html work

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// starter data for friends will add new inputs
+
+const friendsArray = [
+  {
+    name: "Plato",
+    photo: "https://www.ancient.eu/img/r/p/500x600/1165.jpg?v=1485680401",
+    scores: [
+      5,
+      3,
+      2,
+      4,
+      1,
+      3,
+      4,
+      5,
+      5,
+      1
+    ]
+  },
+]
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+// Import array data
+
+const friendsData = require("../data/friends");
+
+// routing 
+
+module.exports = function(app) {
+  
+}
